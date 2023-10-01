023472e8d0d73957a2a96163b095648d85352bf4
netchatout <146669007+netchatout@users.noreply.github.com>
2023-10-01 21:06:32 +0100

    Update wip_index.php
    Signed-off-by: netchatout <146669007+netchatout@users.noreply.github.com>

--- wip_index.php
+++ wip_index.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
+<?php
+session_start();
+if(!isset($_SESSION["username"])){
+header("Location: login.php");
+exit(); }
+?>
 <html>
-  <head>
-    <title>Net ChatOut</title>
-    <!--
-      owebscr~
-        Prefix: {
-          headline: OWeb
-          reader: OWeb
-          depiction: OWeb
-        }
-      ~owebscr
-      otitl=pLoad
-      oscpt=disabled-webrun-false
-    !-->
-  </head>
-  <body>
-    <h1>ChatOut is still a work in progress!</h1>
-    <p>But don't be disappointed! The first WIP build is to be released in less than a month!</p>
-  </body>
+<p>the page to redirected to after login</p>
 </html>
