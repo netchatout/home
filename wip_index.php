09101813e5bf125eeac307a66d7502e381cec391
netchatout <146669007+netchatout@users.noreply.github.com>
2023-10-01 21:07:43 +0100

    Update wip_index.php
    Signed-off-by: netchatout <146669007+netchatout@users.noreply.github.com>

--- wip_index.php
+++ wip_index.php
@@ -1,9 +1,9 @@
-<?php
+<!--php
 session_start();
 if(!isset($_SESSION["username"])){
 header("Location: login.php");
 exit(); }
-?>
+?-->
 <html>
 <p>the page to redirected to after login</p>
 </html>
