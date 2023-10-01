7270d1673814d5b476cf93c88ab53964ce57d1bf
netchatout <146669007+netchatout@users.noreply.github.com>
2023-10-01 21:06:50 +0100

    Update index.php
    Signed-off-by: netchatout <146669007+netchatout@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
-<?php
-session_start();
-if(!isset($_SESSION["username"])){
-header("Location: login.php");
-exit(); }
-?>
+<!DOCTYPE html>
 <html>
-<p>the page to redirected to after login</p>
+  <head>
+    <title>Net ChatOut</title>
+    <!--
+      owebscr~
+        Prefix: {
+          headline: OWeb
+          reader: OWeb
+          depiction: OWeb
+        }
+      ~owebscr
+      otitl=pLoad
+      oscpt=disabled-webrun-false
+    !-->
+  </head>
+  <body>
+    <h1>ChatOut is still a work in progress!</h1>
+    <p>But don't be disappointed! The first WIP build is to be released in less than a month!</p>
+  </body>
 </html>
